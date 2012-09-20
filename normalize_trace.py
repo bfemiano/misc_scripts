5fefccfc1bfdba908203e1d18eb92d86c0303c7d
Brian,Femiano <bfemiano@gmail.com>
2012-09-20 14:29:46 -0400

    updated to produce different files for type)

--- normalize_trace.py
+++ normalize_trace.py
@@ -2,46 +2,49 @@ import csv
 import sys
 import os
 
-def output(writer, outfields, items, index, offset):
-	for i in range(0, (len(items)-offset)):
-		outfields.pop(index+i)
-		outfields.insert(index+i, items[i+offset])
-	writer.writerow(outfields)
-
 #Takes a tracefile and expands into multilple lines for each memory-read, memory-write, register read and/or register write per instruction. 
 # outputs a tableau ready file to the working directory where the script was executed. 
 # requires relative or absolute path to a tracefile.  
+
+def output(writer, fields, offset, seq):
+	for i in range(0, offset):
+		fields.pop(0)
+	fields.insert(0, seq)
+	writer.writerow(fields)
+	
+		
 def main(filePath):
-	workingPath = os.path.dirname(filePath[1])
-	reader = csv.reader(open(workingPath + '/trace.final'), delimiter='|')
-	writer = csv.writer(open('./tableau_'+ os.path.basename(filePath[1]) + '.tsv', 'wb'), delimiter='\t')
-	writer.writerow(['seq', 'td', 'ip', #base 3 fields
-	'mr_addr', 'mr_size', 'mr_value', #memory read 
-	'mw_addr', 'mw_size', 'mw_value', #memory write
-	'rr_reg', 'rr_val', #register read 
-	'wr_reg', 'wr_val']) #register write
+	workingPath = os.path.dirname(filePath)
+	reader = csv.reader(open(filePath), delimiter='|')
+	basicOut = csv.writer(open(workingPath + '/tableau_basic_info.tsv', 'wb'), delimiter='\t')
+	memReadOut = csv.writer(open(workingPath + '/tableau_memread.tsv', 'wb'), delimiter='\t')
+	memWriteOut = csv.writer(open(workingPath + '/tableau_memwrite.tsv', 'wb'), delimiter='\t')
+	regReadOut = csv.writer(open(workingPath + '/tableau_regread.tsv', 'wb'), delimiter='\t')
+	regWriteOut = csv.writer(open(workingPath + '/tableau_regwrite.tsv', 'wb'), delimiter='\t')
+	basicOut.writerow(['seq', 'td', 'ip']), #base 3 fields
+	memReadOut.writerow(['seq', 'mr_addr', 'mr_size', 'mr_value']) #memory read 
+	memWriteOut.writerow(['seq', 'mw_addr', 'mw_size', 'mw_value']) #memory write
+	regReadOut.writerow(['seq', 'rr_reg', 'rr_val']) #register read 
+	regWriteOut.writerow(['seq', 'wr_reg', 'wr_val']) #register write
 	for row in reader:
-		outfields = [row[0], row[1], row[2]]
-		for i in range(0,10):
-			outfields.append('')
+		basicOut.writerow([row[0], row[1], row[2]])
 		memread = row[5]
 		if len(memread) > 0:
-			items = memread.split(':')
-			output(writer, [x for x in outfields], items, 3, 2)
+			output(memReadOut, memread.split(':'), 2, row[0])
 		memwrite = row[6]
 		if len(memwrite) > 0:
 			items = memwrite.split(':')
 			if len(items) == 4:
 				items.append('')
-			output(writer, [x for x in outfields], items, 6, 2)
+			output(memWriteOut, items, 2, row[0])
 		rreads = row[7]
 		if len(rreads) > 0:
 			for rread in rreads.split(','):
-				output(writer, [x for x in outfields], rread.split(':'), 9, 1)
+				output(regReadOut, rread.split(':'), 1, row[0])
 		rwrites = row[8]
 		if len(rwrites) > 0:
 			for rwrite in rwrites.split(','):
-				output(writer, [x for x in outfields], rwrite.split(':'), 11, 1)
+				output(regWriteOut, rwrite.split(':'), 1, row[0])
 
 if __name__ == "__main__":
-	main(sys.argv)
+	main(sys.argv[1])
