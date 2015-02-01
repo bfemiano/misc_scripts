4d2566539dee11c457311c64f6c137a71550eab8
bfemiano <bfemiano@gmail.com>
2015-02-01 15:47:30 -0500

    modified sort script to use list comprehensions and inline sort lambda

--- sort_lift.py
+++ sort_lift.py
@@ -1,20 +1,13 @@
 import csv
 from itertools import groupby
 
-def sort_by(x):
-	(dt, p, l1, l2, l3, l, index) = x
-	return int(p)
-
 with open('/Users/bfemiano/category_lift.csv000', 'r') as lift_file:
 	items = []
 	for line in lift_file.readlines():
 		items.append(line.strip('\n\r').split(','))
-	items = sorted(items, key=sort_by, reverse=True)
+	items = sorted(items, key=lambda x: int(x[1]), reverse=True)
 	with open('sorted_lift_by_score.csv', 'wb') as out_file:
 		writer = csv.writer(out_file, delimiter=',')
-		groups = []
-		for k,grp in groupby(items, lambda x: int(x[1])):
-			 groups.append(list(grp))
+		groups = [list(grp) for k,grp in groupby(items, lambda x: int(x[1]))]
 		for g in groups:
-			g_s = sorted(g, key=lambda x : int(x[6]), reverse=True)
-			writer.writerows(g_s)
+			writer.writerows(sorted(g, key=lambda x : int(x[6]), reverse=True))
