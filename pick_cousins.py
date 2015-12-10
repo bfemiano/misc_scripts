6333d16eb08d7074e6fd5bfd425b1cef51221c18
bfemiano <brian.femiano@magnetic.com>
2015-12-09 21:22:45 -0500

    added blocked cousins feature

--- pick_cousins.py
+++ pick_cousins.py
@@ -1,15 +1,15 @@
 import random
+import sys
 import smtplib
 import time
 from getpass import getpass
 
-
 #File of the form,
-# name1, email1
-# name2, email2
+# name1, email1, blocked_name|another_blocked_name
+# name2, email2, blocked_name|another_blocked_name
 # not checked in to hide email addresses. 
 cousins = []
-with open('name_email_file.txt', 'r') as name_email_pairs_in:
+with open('cousin_names_and_emails.txt', 'r') as name_email_pairs_in:
     for line in name_email_pairs_in.readlines():
         cousins.append(tuple(line.strip('\n').split(',')))
 
@@ -28,8 +28,8 @@ def generate_email(giver, receiver):
     '''
         Programatically build the email message and return it as a string. 
     '''
-    (giver_name, giver_email) = giver
-    (receiver_name, receiver_email) = receiver
+    (giver_name, giver_email, blocked1) = giver
+    (receiver_name, receiver_email, blocked2) = receiver
     msg = '''Subject: Here's your assigned cousin!\n
     
     Hello {giver_name},
@@ -47,14 +47,26 @@ def generate_email(giver, receiver):
 cousins_copy = [c for c in cousins]
 emails_to_send = []
 
+i = 0
 for c in cousins_copy:
-    choice = 'N'
-    while choice == 'N': #Keep picking until we are satisfied with the selection. This is where we can prevent cousins of immediate family from being paired together. 
+    pair_next_cousin = True
+    while pair_next_cousin: 
         rand_cousin = pick(c)
-        choice = raw_input("Accept %s -> %s? Enter (Y/N) " % (c, rand_cousin))
+        (giver_name, giver_email, giver_blocked) = c
+        (receipiant_name, receipiant_email, receipiant_blocked) = rand_cousin
+        pair_next_cousin = receipiant_name in set(giver_blocked.strip(' ').split('|'))
+        i += 1
+        if i > 100000:
+            print 'Cousin blocked list made pairings impossible this round, considering losing some of the blocked names per cousin and try again.'
+            sys.exit(0)
+    print '%s --> %s' % (giver_name, receipiant_name)
     cousins.remove(rand_cousin)
     emails_to_send.append(generate_email(c, rand_cousin))
 
+contin = raw_input("Do you approve of the list? (Only 'Y' will continue)")
+if contin != 'Y':
+    print 'Aborting'
+    sys.exit(0)
 fastmail_server = 'mail.messagingengine.com:587'
 username = 'bfemiano@fastmail.com'
 password = getpass("password please ")
@@ -66,6 +78,7 @@ try:  #email out all the cousins using the prepared message.
     for (giver_email, msg) in emails_to_send:
         print 'sending email to %s' % giver_email
         server.sendmail(username, giver_email, msg)
-        time.sleep(3) #rate limit to avoid being flagged as spam, just as a precaution.    
+        time.sleep(3) #rate limit to avoid being flagged as spam, just as a precaution.  
+    print 'Success!'  
 finally:
     server.quit()
