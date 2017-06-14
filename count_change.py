ca90e805aad3ecf1d8c97fb5c718b0c07c01dd1f
Brian Femiano <bfemiano@gmail.com>
2017-06-14 00:40:55 -0400

    better solution for count change.

--- count_change.py
+++ count_change.py
@@ -1,22 +1,17 @@
 import sys
 
-def count_change_inner(money, coins, combos, active):
-    if len(coins) == 0:
-        return 0
-    if not (tuple(sorted(active)), False) in combos:
-        if money <= 0:
-            if money == 0:
-                combos.add((tuple(sorted(active)), True))
-            return combos
-        combos.add((tuple(sorted(active)), False))
-
-        def _count(coin):
-            count_change_inner(money-coin, coins, combos, active + [coin])
-        map(_count, coins)
-    return combos
-
-
 def count_change(money, coins):
-    return len(filter(lambda c: c[1], count_change_inner(money, filter(lambda x: x <= money, coins), set(), [])))
 
-print count_change(int(sys.argv[1]), list(reversed(sorted(map(int, sys.argv[2].split(','))))))
+    def _count(money, coins, cnt):
+        if len(coins) == 0 or money < 0:
+            return cnt
+        if money == 0:
+            return cnt + 1
+
+        cnt = _count(money-coins[0], coins, cnt)
+        cnt = _count(money, coins[1:], cnt)
+        return cnt
+
+    return _count(money, sorted(coins), 0)
+
+print count_change(int(sys.argv[1]), map(int, sys.argv[2].split(',')))
