ec88978581d5d7af8a8d332ed68ceb41800ac04d
Brian Femiano <bfemiano@gmail.com>
2017-07-24 09:40:23 -0400

    rename

--- n_dice.py
+++
@@ -1,9 +0,0 @@
-def ndice(n, seq=""):
-    if n == 1:
-        for i in range(1, 7):     
-            print seq + str(i)
-    else:
-        for i in range(1, 7):
-            ndice(n-1, seq + str(i))        
-
-ndice(3) #prints all 216 combintions. 
