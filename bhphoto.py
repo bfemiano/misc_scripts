0a1db9501296bc542a654446fe67f9a21d251572
Brian Femiano <bfemiano@gmail.com>
2020-12-04 23:03:34 -0500

    email formatted ok. app password redacted.

--- bhphoto.py
+++ bhphoto.py
@@ -15,28 +15,30 @@ headers = {
 
 }
 
-url = "https://www.bhphotovideo.com/api/item/p/product-details?from=cli&aperture=1&cliReqId=a41c1e91-9ef2c6-a409-2105d52f7b22-cli-7"
+link = "https://www.bhphotovideo.com/c/product/1595780-REG/pny_technologies_vcg308010tfxmpb_geforce_rtx_3080_10gb.html"
 
-req = request.Request(url, values, headers)
+api_url = "https://www.bhphotovideo.com/api/item/p/product-details?from=cli&aperture=1&cliReqId=a41c1e91-9ef2c6-a409-2105d52f7b22-cli-7"
+
+req = request.Request(api_url, values, headers)
 recpt = "bfemiano@fastmail.com"
 with request.urlopen(req) as response:
     data_back = json.loads(response.read())
     for item in data_back['data']:
         can_add_to_cart = item['priceInfo']['addToCartButton']
         name = item['core']['shortDescription']
-        if can_add_to_cart == 'NONE' and not os.path.exists("/Users/bfemiano/bhphoto_email.sent"):
+        if can_add_to_cart != 'NONE' and not os.path.exists("/Users/bfemiano/bhphoto_email.sent"):
             gmail_server = 'smtp.gmail.com'
             username = 'bfemiano@gmail.com'
-            password = "get_from_app"
+            password = "REPLACE WITH REAL APP PASSWORD"
             server = smtplib.SMTP(gmail_server, 587)
             server.starttls()
             server.login(username,password)
 
             msg = """From: bfemiano@gmail.com\nTo: {recpt}\nSubject: BH Photo might have your item in stock!\n
 
-                {name}
+                {name}\n\n{url}
 
-            """.format(recpt=recpt, name=name)
+            """.format(recpt=recpt, url=link, name=name)
 
             server.sendmail(username, recpt, msg)
             with open("/Users/bfemiano/bhphoto_email.sent", 'w') as foo:
