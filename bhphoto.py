2d7882ae5a2201b8b8dea5be8c354909aa60242c
Brian Femiano <bfemiano@gmail.com>
2020-12-05 19:46:17 -0500

    uniform wait seconds. added crash notification.

--- bhphoto.py
+++ bhphoto.py
@@ -6,6 +6,7 @@ import os
 import smtplib
 from datetime import datetime
 from time import sleep
+from random import uniform
 
 values = b'{"params":{"itemList":[{"skuNo":1606947,"itemSource":"REG"}],"channels":["priceInfo"],"channelParams":{"priceInfo":{"PRICING_CONTEXT":"DETAILS_CART_LAYER"}}}}'
 
@@ -18,7 +19,7 @@ headers = {
 
 }
 
-recpts = ["ADD EMAILS"]
+recpts = ["ALL_EMAILS"]
 
 link = "https://www.bhphotovideo.com/c/product/1606947-REG/gigabyte_gv_n306tgaming_oc_8gd_rtx_3060_ti_gaming.html"
 
@@ -29,32 +30,44 @@ req = request.Request(api_url, values, headers)
 if os.path.exists("/Users/bfemiano/bhphoto_email.sent"):
     print("sent file already exists. Mistake?")
 
+gmail_server = 'smtp.gmail.com'
+username = 'REDACDTED'
+password = "REDACTED"
+server = smtplib.SMTP(gmail_server, 587)
+server.starttls()
+server.login(username,password)
+
 while(True):
-    with request.urlopen(req) as response:
-        data_back = json.loads(response.read())
-        for item in data_back['data']:
-            can_add_to_cart = item['priceInfo']['addToCartButton']
-            add_to_crt_fnc = item['priceInfo']['addToCartFunction']
-            name = item['core']['shortDescription']
-            if (can_add_to_cart != 'NONE' or add_to_crt_fnc is not None) \
-                    and not os.path.exists("/Users/bfemiano/bhphoto_email.sent"):
-                gmail_server = 'smtp.gmail.com'
-                username = 'REDACTED'
-                password = "REDACTED"
-                server = smtplib.SMTP(gmail_server, 587)
-                server.starttls()
-                server.login(username,password)
-                dt = datetime.now()
-                dt_str = dt.strftime("%Y-%m-%d %H:%M")
-                for recpt in recpts:
-                    print("Sending email to %s at %s" % (recpt, dt_str))
-                    msg = """From: REDACTED\nTo: {recpt}\nSubject: BH Photo might have your item in stock!\n
+    try:
+        with request.urlopen(req) as response:
+            data_back = json.loads(response.read())
+            for item in data_back['data']:
+                can_add_to_cart = item['priceInfo']['addToCartButton']
+                add_to_crt_fnc = item['priceInfo']['addToCartFunction']
+                name = item['core']['shortDescription']
+                if (can_add_to_cart != 'NONE' or add_to_crt_fnc is not None) \
+                        and not os.path.exists("/Users/bfemiano/bhphoto_email.sent"):
+                    dt = datetime.now()
+                    dt_str = dt.strftime("%Y-%m-%d %H:%M")
+                    for recpt in recpts:
+                        print("Sending email to %s at %s" % (recpt, dt_str))
+                        msg = """From: REDACTED\nTo: {recpt}\nSubject: BH Photo might have your item in stock!\n
 
-                    {name}\n\n{url}
+                        {name}\n\n{url}
 
-                    """.format(recpt=recpt, url=link, name=name)
+                        """.format(recpt=recpt, url=link, name=name)
 
-                    server.sendmail(username, recpt, msg)
-                with open("/Users/bfemiano/bhphoto_email.sent", 'w') as foo:
-                    pass
-    sleep(5)
+                        server.sendmail(username, recpt, msg)
+                    with open("/Users/bfemiano/bhphoto_email.sent", 'w') as foo:
+                        pass
+        lag = uniform(5,20)
+        sleep(lag)
+    except:
+        raise
+    finally:
+        msg = """From: REDACTED\nTo: {recpt}\nSubject: Your BH photo script crashed!\n
+
+                Your script crashed
+
+               """.format(recpt="REDACTED")
+        server.sendmail(username, "REDACTED", msg)
