126bd069899892de40d955a8307163b681a5e0d3
Brian Femiano <bfemiano@gmail.com>
2021-03-20 04:29:30 -0400

    word combination printout for letters

---
+++ word_combinations.py
@@ -0,0 +1,12 @@
+letters_start = ["N", "U", "A", "T"]
+def p(letters, word):
+    for i in range(len(letters)):
+          if len(letters) > 1:	
+              word.append(letters[i])
+              p([l for l in letters if l != letters[i]], word)
+              word.pop()
+          else:
+              print(''.join(word + letters))
+
+p(letters_start, [])
+     
