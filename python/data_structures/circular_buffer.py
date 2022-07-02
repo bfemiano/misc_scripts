8c8d80b00bfdfc0e704bd8032fe2c9ff09cdab26
Brian Femiano <brianfemiano@Brians-MacBook-Pro.local>
2022-07-02 00:21:36 -0400

    fixed circular buffer bug. added lousy bloom filter.

--- python/data_structures/circular_buffer.py
+++ python/data_structures/circular_buffer.py
@@ -1,29 +1,29 @@
 
 class CircularBuffer(object):
     """
-        Design a data structure structure with an initial fixed capacity, 
-        where elements are retrived in the order they are inserted. 
-        If the structure reaches it's capacity, then overwrites the oldest data
-        in the structure
+        Implement a data structure structure with the following properties:
+        1. Initialized to a fixed capacity.
+        2. Elements are retrived in the order they are inserted. 
+        3. If the structure reaches it's capacity, then overwrite the oldest data in the structure.
 
         For example:
         capacity = 4
         items inserted: 1 being the oldest, 4 being the most recent. 
             [1, 2, 3, 4]
-        insert 5 would produce:
+        inserting 5 would produce:
             [5, 2, 3, 4]
-        insert 6 would produce:
+        inserting 6 would produce:
             [5, 6, 3, 4]
+        read would then return 3
 
-
-       Here's the operations our structure can perform:
+       Here's the operations the structure should perform:
         1. read(). Retrieve and remove the oldest item inserted into the data structure. If the structure is empty, return -1. 
         2. write(item). Insert an item in the data structure. 
         3. clear(). After this method is called, calls to read() return -1 to signal empty until new writes occur.   
     """
 
     def __init__(self, capacity):
-        self.items = [0 for i in range(capacity)] # initialize to empty
+        self.items = [-1 for i in range(capacity)] # initialize to empty
         self.max_size = capacity
         self.size = 0
         self.read_ptr = 0
@@ -40,6 +40,8 @@ class CircularBuffer(object):
         return val
 
     def write(self, item):
+        if self.write_ptr == self.read_ptr and self.size > 0: # as we overwrite the oldest data, keep the read pointer moving. 
+            self.read_ptr += 1
         self.items[self.write_ptr] = item
         self.size += 1
         self.write_ptr += 1
@@ -80,8 +82,8 @@ def test_buffer_overflow_causes_circular_wrap():
     buffer.write(2)
     buffer.write(3)
     buffer.write(4)
-    buffer.write(5) # capacity = 4 means 5th insert overwrites oldest insert.
-    assert buffer.read() == 5
+    buffer.write(5) # capacity = 4 means 5th insert overwrites oldest insert. Now oldest insert is 2
+    assert buffer.read() == 2
 
 def test_clear():
     buffer = CircularBuffer(4)
---
+++ python/data_structures/lousy_bloom_filter.py
@@ -0,0 +1,46 @@
+class LousyBloomFilter(object):
+    """
+        Let's implement a data structure that checks for hash containment in constant time without having to store all the hashes. 
+        To accomplish this, we're allowed the potential for slight error. 
+        
+        The structure can sometimes say a hash is present when it's actually not (false positive), 
+           but it should never say a hash wasn't present when it actually was (false negative).
+
+        Assume hashs are integers >= 0. 
+        
+        Functions: 
+            1. add(hash)
+                Add the hash to the structure. 
+
+            1. contains(hash)
+                If it returns True, there's a chance it might not actually contain the hash.  (False positive)
+                If it returns False, then it definitely did not contain the hash. 
+    """
+
+    def __init__(self, capacity):
+        self.bit_vector = [0 for _ in range(capacity)]
+        self.capacity = capacity
+
+    def add(self, hash):
+        self.bit_vector[hash % self.capacity] = 1
+
+    def contains(self, hash):
+        return bool(self.bit_vector[hash % self.capacity])
+
+def test_no_collisions_means_100perc_accuracy():
+    bloom = LousyBloomFilter(5)
+    bloom.add(0)
+    bloom.add(4)
+    assert bloom.bit_vector == [1, 0, 0, 0, 1]
+    assert bloom.contains(0)
+    assert bloom.contains(4)
+    assert not bloom.contains(1)
+
+def test_false_positives_when_collisions():
+    bloom = LousyBloomFilter(5)
+    bloom.add(0)
+    bloom.add(6)
+    assert bloom.bit_vector == [1, 1, 0, 0, 0] # 6 was indexed at 1 in the vector, so checking for 1 will now yield a false positive.
+    assert bloom.contains(0)
+    assert bloom.contains(6)
+    assert bloom.contains(1) # false positive. Allowable for bloom filter. 
