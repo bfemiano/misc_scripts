cebc097c0a6681c2f685d5b8b94ddbdaf6312e14
Brian Femiano <brianfemiano@Brians-MacBook-Pro.local>
2022-07-05 09:31:40 -0400

    lousy bloom to store booleans

--- python/data_structures/lousy_bloom_filter.py
+++ python/data_structures/lousy_bloom_filter.py
@@ -18,20 +18,20 @@ class LousyBloomFilter(object):
     """
 
     def __init__(self, capacity):
-        self.bit_vector = [0 for _ in range(capacity)]
+        self.bit_vector = [False for _ in range(capacity)]
         self.capacity = capacity
 
     def add(self, hash):
-        self.bit_vector[hash % self.capacity] = 1
+        self.bit_vector[hash % self.capacity] = True
 
     def contains(self, hash):
-        return bool(self.bit_vector[hash % self.capacity])
+        return self.bit_vector[hash % self.capacity]
 
 def test_no_collisions_means_100perc_accuracy():
     bloom = LousyBloomFilter(5)
     bloom.add(0)
     bloom.add(4)
-    assert bloom.bit_vector == [1, 0, 0, 0, 1]
+    assert list(map(int, bloom.bit_vector)) == [1, 0, 0, 0, 1]
     assert bloom.contains(0)
     assert bloom.contains(4)
     assert not bloom.contains(1)
@@ -40,7 +40,7 @@ def test_false_positives_when_collisions():
     bloom = LousyBloomFilter(5)
     bloom.add(0)
     bloom.add(6)
-    assert bloom.bit_vector == [1, 1, 0, 0, 0] # 6 was indexed at 1 in the vector, so checking for 1 will now yield a false positive.
+    assert list(map(int, bloom.bit_vector)) == [1, 1, 0, 0, 0] # 6 was indexed at 1 in the vector, so checking for 1 will now yield a false positive.
     assert bloom.contains(0)
     assert bloom.contains(6)
     assert bloom.contains(1) # false positive. Allowable for bloom filter. 
